43114314e54ab8d1acbb691c94d33281b74644a6
MinerSebas <scherthan_sebastian@web.de>
2020-06-15 17:04:49 +0200

    Fixed inability to research tech requiring specific facilities.

---
+++ Source/Extension_ResearchProjectDef.cs
@@ -0,0 +1,45 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace HumanResources
+{
+    public static class Extension_ResearchProjectDef
+	{
+		public static bool CanBeResearchedAt(this ResearchProjectDef researchProjectDef, Building_WorkTable bench, bool ignoreResearchBenchPowerStatus)
+		{
+			if (researchProjectDef.requiredResearchBuilding != null && bench.def != researchProjectDef.requiredResearchBuilding)
+			{
+				return false;
+			}
+			if (!ignoreResearchBenchPowerStatus)
+			{
+				CompPowerTrader comp = bench.GetComp<CompPowerTrader>();
+				if (comp != null && !comp.PowerOn)
+				{
+					return false;
+				}
+			}
+			if (!researchProjectDef.requiredResearchFacilities.NullOrEmpty<ThingDef>())
+			{
+				CompAffectedByFacilities affectedByFacilities = bench.TryGetComp<CompAffectedByFacilities>();
+				if (affectedByFacilities == null)
+				{
+					return false;
+				}
+				List<Thing> linkedFacilitiesListForReading = affectedByFacilities.LinkedFacilitiesListForReading;
+				int j;
+				int i;
+				for (i = 0; i < researchProjectDef.requiredResearchFacilities.Count; i = j + 1)
+				{
+					if (linkedFacilitiesListForReading.Find((Thing x) => x.def == researchProjectDef.requiredResearchFacilities[i] && affectedByFacilities.IsFacilityActive(x)) == null)
+					{
+						return false;
+					}
+					j = i;
+				}
+			}
+			return true;
+		}
+	}
+}
---
+++ Source/Harmony/ResearchProjectDef_Patches.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace HumanResources
+{
+	[HarmonyPatch(typeof(ResearchProjectDef))]
+	public static class ResearchProjectDef_Patches
+	{
+		[HarmonyPrefix]
+		[HarmonyPatch("PlayerHasAnyAppropriateResearchBench", MethodType.Getter)]
+		public static bool PlayerHasAnyAppropriateResearchBench_Prefix(ResearchProjectDef __instance, ref bool __result)
+		{
+			List<Map> maps = Find.Maps;
+			for (int i = 0; i < maps.Count; i++)
+			{
+				List<Building> allBuildingsColonist = maps[i].listerBuildings.allBuildingsColonist;
+				for (int j = 0; j < allBuildingsColonist.Count; j++)
+				{
+					Building_WorkTable building_ResearchBench = allBuildingsColonist[j] as Building_WorkTable;
+					if (building_ResearchBench != null && __instance.CanBeResearchedAt(building_ResearchBench, true))
+					{
+						__result = true;
+						return false;
+					}
+				}
+			}
+			__result = false;
+			return false;
+		}
+	}
+}
