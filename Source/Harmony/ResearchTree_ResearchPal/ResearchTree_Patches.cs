7fa16f84603cb638b37171cd760f8d3b01b9ecbf
João Pedro Torres <jaopedro05@outlook.com>
2020-06-16 01:54:19 -0300

    Trying to solve research bench presence check, partial success.

--- Source/Harmony/ResearchTree_ResearchPal/ResearchTree_Patches.cs
+++ Source/Harmony/ResearchTree_ResearchPal/ResearchTree_Patches.cs
@@ -1,6 +1,9 @@
 ﻿using HarmonyLib;
+using RimWorld;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using UnityEngine;
 using Verse;
 
@@ -19,23 +22,27 @@ namespace HumanResources
 
             //ResearchProjectDef_Extensions
             instance.CreateReversePatcher(AccessTools.Method(modName + ".ResearchProjectDef_Extensions:GetUnlockDefsAndDescs"),
-                new HarmonyMethod(AccessTools.Method(typeof(ResearchTree_Patches), nameof(ResearchTree_Patches.GetUnlockDefsAndDescs)))).Patch();
+                new HarmonyMethod(AccessTools.Method(typeof(ResearchTree_Patches), nameof(GetUnlockDefsAndDescs)))).Patch();
             instance.CreateReversePatcher(AccessTools.Method(modName + ".ResearchProjectDef_Extensions:GetRecipesUnlocked"),
-                new HarmonyMethod(AccessTools.Method(typeof(ResearchTree_Patches), nameof(ResearchTree_Patches.GetRecipesUnlocked)))).Patch();
+                new HarmonyMethod(AccessTools.Method(typeof(ResearchTree_Patches), nameof(GetRecipesUnlocked)))).Patch();
             instance.CreateReversePatcher(AccessTools.Method(modName + ".ResearchProjectDef_Extensions:GetThingsUnlocked"),
-                new HarmonyMethod(AccessTools.Method(typeof(ResearchTree_Patches), nameof(ResearchTree_Patches.GetThingsUnlocked)))).Patch();
+                new HarmonyMethod(AccessTools.Method(typeof(ResearchTree_Patches), nameof(GetThingsUnlocked)))).Patch();
             instance.CreateReversePatcher(AccessTools.Method(modName + ".ResearchProjectDef_Extensions:GetPlantsUnlocked"),
-                new HarmonyMethod(AccessTools.Method(typeof(ResearchTree_Patches), nameof(ResearchTree_Patches.GetPlantsUnlocked)))).Patch();
+                new HarmonyMethod(AccessTools.Method(typeof(ResearchTree_Patches), nameof(GetPlantsUnlocked)))).Patch();
+            instance.CreateReversePatcher(AccessTools.Method(modName + ".ResearchProjectDef_Extensions:Ancestors"),
+                new HarmonyMethod(AccessTools.Method(typeof(ResearchTree_Patches), nameof(Ancestors)))).Patch();
 
             //ResearchNode
             instance.CreateReversePatcher(AccessTools.Method(modName + ".ResearchNode:BuildingPresent", new Type[] { typeof(ResearchProjectDef) }),
-                new HarmonyMethod(AccessTools.Method(typeof(ResearchTree_Patches), nameof(ResearchTree_Patches.BuildingPresent)))).Patch();
+                new HarmonyMethod(AccessTools.Method(typeof(ResearchTree_Patches), nameof(BuildingPresent)))).Patch();
+            instance.Patch(AccessTools.Method(modName + ".ResearchNode:BuildingPresent", new Type[] { typeof(ResearchProjectDef) }),
+                null, new HarmonyMethod(typeof(ResearchTree_Patches), nameof(BuildingPresent_Postfix)), null);
             instance.CreateReversePatcher(AccessTools.Method(modName + ".ResearchNode:MissingFacilities", new Type[] { typeof(ResearchProjectDef) }),
-                new HarmonyMethod(AccessTools.Method(typeof(ResearchTree_Patches), nameof(ResearchTree_Patches.MissingFacilities)))).Patch();
+                new HarmonyMethod(AccessTools.Method(typeof(ResearchTree_Patches), nameof(MissingFacilities)))).Patch();
 
             //Def_Extensions
             instance.CreateReversePatcher(AccessTools.Method(modName + ".Def_Extensions:DrawColouredIcon"),
-                new HarmonyMethod(AccessTools.Method(typeof(ResearchTree_Patches), nameof(ResearchTree_Patches.DrawColouredIcon)))).Patch();
+                new HarmonyMethod(AccessTools.Method(typeof(ResearchTree_Patches), nameof(DrawColouredIcon)))).Patch();
         }
 
         public static List<Pair<Def, string>> GetUnlockDefsAndDescs(ResearchProjectDef research, bool dedupe = true) { throw stubMsg; }
@@ -45,6 +52,35 @@ namespace HumanResources
         public static IEnumerable<RecipeDef> GetRecipesUnlocked(this ResearchProjectDef research) { throw stubMsg; }
         public static IEnumerable<ThingDef> GetThingsUnlocked(this ResearchProjectDef research) { throw stubMsg; }
         public static IEnumerable<ThingDef> GetPlantsUnlocked(this ResearchProjectDef research) { throw stubMsg; }
+        public static List<ResearchProjectDef> Ancestors(this ResearchProjectDef research) { throw stubMsg; }
+
+        private static FieldInfo buildingPresentCacheInfo = AccessTools.Field(ResearchNodeType(), "_buildingPresentCache");
+        //private static MethodInfo buildingPresentCacheSetMethod => buildingPresentCacheInfo.FieldType.GetMethod("set_Item", new Type[] { typeof(ResearchProjectDef), typeof(bool) });
+
+        public static void BuildingPresent_Postfix(ResearchProjectDef research, ref bool __result)
+        {
+            bool flag = false;
+            if (!__result && research.requiredResearchBuilding != null)
+            {
+                flag = Find.Maps.SelectMany((Map map) => map.listerBuildings.allBuildingsColonist).OfType<Building_WorkTable>().Any((Building_WorkTable b) => research.Alt_CanBeResearchedAt(b));
+            }
+            if (flag)
+            {
+                flag = research.Ancestors().All(new Func<ResearchProjectDef, bool>(BuildingPresent));
+            }
+            string test = buildingPresentCacheInfo != null ? "ok" : "bad";
+            Log.Warning("DEBUG buildingPresentCacheInfo is " + test);
+            //FieldInfo buildingPresentCacheInfo = AccessTools.Field(ResearchNodeType(), "_buildingPresentCache");
+            MethodInfo buildingPresentCacheSetMethod = buildingPresentCacheInfo.FieldType.GetMethod("set_Item");
+            //object dict = buildingPresentCacheInfo.GetValue(ResearchNodeType());
+            //buildingPresentCacheSetMethod.Invoke(dict, new object[] { research, flag });
+            
+            string test2 = buildingPresentCacheSetMethod != null ? "ok" : "bad";
+            Log.Warning("DEBUG buildingPresentCacheSetMethod is " + test2);
+
+
+            __result = flag;
+        }
     }
 }
 
