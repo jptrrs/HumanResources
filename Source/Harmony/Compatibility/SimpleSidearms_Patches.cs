6b36cf0cd2574ce48158bfab97489f5d22b7533e
João Pedro Torres <jaopedro05@outlook.com>
2020-07-04 13:23:43 -0300

    Additional known weapon checks, improved support for Simple Sidearms and Awesome Inventory, fixes #15, fixes #42.

--- Source/Harmony/Compatibility/SimpleSidearms_Patches.cs
+++ Source/Harmony/Compatibility/SimpleSidearms_Patches.cs
@@ -16,16 +16,24 @@ namespace HumanResources
         {
             Type StatCalculatorType = AccessTools.TypeByName("SimpleSidearms.utilities.StatCalculator");
             MethodBase canCarrySidearmBase = AccessTools.Method(StatCalculatorType, "canCarrySidearmInstance", new Type[] { typeof(ThingWithComps), typeof(Pawn), typeof(string).MakeByRefType() });
-
             instance.Patch(AccessTools.Method(StatCalculatorType, "canCarrySidearmInstance", new Type[] { typeof(ThingWithComps), typeof(Pawn), typeof(string).MakeByRefType() }),
                 new HarmonyMethod(typeof(SimpleSidearms_Patches), nameof(canCarrySidearmÌnstance_Prefix)), null, null);
+            
+            Type WeaponAssingmentType = AccessTools.TypeByName("SimpleSidearms.utilities.WeaponAssingment");
+            instance.Patch(AccessTools.Method(WeaponAssingmentType, "equipSpecificWeapon"),
+                new HarmonyMethod(typeof(SimpleSidearms_Patches), nameof(equipSpecificWeapon_Prefix)), null, null);
         }
 
         public static bool canCarrySidearmÌnstance_Prefix(ThingWithComps sidearmThing, Pawn pawn, out string errString)
         {
-            Log.Warning("kicking in");
             errString = ModBaseHumanResources.unlocked.weapons.Contains(sidearmThing.def) ? "UnknownWeapon".Translate(pawn) : "EvilWeapon".Translate(pawn);
             if (pawn.RaceProps.Humanlike && pawn.Faction.IsPlayer && pawn.TryGetComp<CompKnowledge>() != null) return HarmonyPatches.CheckKnownWeapons(pawn, sidearmThing);
+            else return true;
+        }
+
+        public static bool equipSpecificWeapon_Prefix(Pawn pawn, ThingWithComps weapon)
+        {
+            if (pawn.RaceProps.Humanlike && pawn.Faction.IsPlayer && pawn.TryGetComp<CompKnowledge>() != null) return HarmonyPatches.CheckKnownWeapons(pawn, weapon);
             else return true;
         }
     }
---
+++ Source/Harmony/ThingOwner_TryAddOrTransfer.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Verse;
+
+namespace HumanResources
+{
+    //Checks if pawn knows a weapon before equiping it from inventory.
+    [HarmonyPatch(typeof(ThingOwner), "TryAddOrTransfer", new Type[] { typeof(Thing), typeof(bool) })]
+    public static class ThingOwner_TryAddOrTransfer
+    {
+        private static FieldInfo pawnInfo = AccessTools.Field(typeof(Pawn_EquipmentTracker), "pawn");
+
+        public static bool Prefix(Thing item, IThingHolder ___owner)
+        {
+            if (item.def.IsWeapon && ___owner is Pawn_EquipmentTracker gear)
+            {
+                Pawn pawn = (Pawn)pawnInfo.GetValue(gear);
+                return HarmonyPatches.CheckKnownWeapons(pawn, item);
+            }
+            return true;
+        }
+    }
+}
