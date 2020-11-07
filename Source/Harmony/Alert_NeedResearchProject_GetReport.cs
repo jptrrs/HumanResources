c6a80be93f172d18667220c6ab14d81f755acb81
João Pedro Torres <jaopedro05@outlook.com>
2020-11-07 14:57:12 -0300

    Supressed "no research project" alert.

---
+++ Source/Harmony/Alert_NeedResearchProject_GetReport.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using RimWorld;
+
+namespace HumanResources
+{
+    //Supress the "Need research project" alert
+    [HarmonyPatch(typeof(Alert_NeedResearchProject), "GetReport")]
+    public static class Alert_NeedResearchProject_GetReport
+    {
+        public static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
