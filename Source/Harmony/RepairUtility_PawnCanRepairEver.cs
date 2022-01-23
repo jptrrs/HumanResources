5c1679877296f74031235fffb696244ed228abd9
João Pedro Torres <jaopedro@gmail.com>
2022-01-22 21:24:08 -0300

    Replaced patch to restrain repair jobs, removed Pawn_JobTracker patch.

---
+++ Source/Harmony/RepairUtility_PawnCanRepairEver.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Linq;
+using Verse;
+using Verse.AI;
+
+namespace HumanResources
+{
+    //Checks if pawn knows how to repair something.
+    [HarmonyPatch(typeof(RepairUtility), nameof(RepairUtility.PawnCanRepairEver), new Type[] { typeof(Pawn), typeof(Thing) })]
+    public static class RepairUtility_PawnCanRepairEver
+    {
+        public static void Postfix(Pawn pawn, Thing t, ref bool __result)
+        {
+            if (__result && pawn.TechBound())
+            {
+                var requisites = t.def.researchPrerequisites;
+                if (requisites.NullOrEmpty()) return;
+                __result = requisites.All(x => x.IsKnownBy(pawn));
+                if (!__result)
+                {
+                    var missing = requisites.Where(x => !x.IsKnownBy(pawn));
+                    string preReqText = (missing.Count() > 1) ? missing.Select(x => x.label).ToStringSafeEnumerable() : missing.FirstOrDefault().label;
+                    JobFailReason.Is("DoesntKnowHowToRepair".Translate(pawn, t.def.label, preReqText));
+                }
+            }
+        }
+    }
+}
