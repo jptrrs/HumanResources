69696a4732f53ce1e874980e5e761ae2c5cfcff7
jptrrs <jaopedro@gmail.com>
2025-02-09 15:54:01 -0300

    Updated SemiRandom compatibility to take the unofficial version.

--- Source/Harmony/Compatibility/SemiRandom_Patch.cs
+++ Source/Harmony/Compatibility/SemiRandom_Patch.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using System;
+using System.Reflection;
 using UnityEngine;
 using Verse;
 
@@ -12,17 +13,27 @@ namespace HumanResources
 
         public static void Execute(Harmony instance)
         {
-            Type Type = AccessTools.TypeByName("CM_Semi_Random_Research.MainTabWindow_NextResearch");
-
+            string root = "CM_Semi_Random_Research";
+            Type Type = AccessTools.TypeByName(root+".MainTabWindow_NextResearch");
             instance.Patch(AccessTools.Method(Type, "DrawRightColumn"),
                 new HarmonyMethod(typeof(SemiRandom_Patch), nameof(DrawRightColumn_Prefix)), new HarmonyMethod(typeof(SemiRandom_Patch), nameof(DrawRightColumn_Postfix)), null);
-            instance.Patch(AccessTools.Method("CM_Semi_Random_Research.ResearchProjectDefExtensions:CanStartProject"),
-                new HarmonyMethod(typeof(SemiRandom_Patch), nameof(CanStartProject_Prefix)), null, null);
-            instance.Patch(AccessTools.Method(typeof(Widgets), "ButtonText", new Type[] { typeof(Rect), typeof(string), typeof(bool), typeof(bool), typeof(bool) }),
-                null, new HarmonyMethod(typeof(SemiRandom_Patch), nameof(ButtonText_Postfix)), null);
+            MethodInfo CanStartMethod = AccessTools.Method(AccessTools.TypeByName(root + ".ResearchProjectDefExtensions"), "CanStartProject");
+            if (CanStartMethod != null)
+            {
+                instance.Patch(AccessTools.Method("CM_Semi_Random_Research.ResearchProjectDefExtensions:CanStartProject"),
+                    new HarmonyMethod(typeof(SemiRandom_Patch), nameof(CanStartProject_Prefix)), null, null);
+                instance.Patch(AccessTools.Method(typeof(Widgets), "ButtonText", new Type[] { typeof(Rect), typeof(string), typeof(bool), typeof(bool), typeof(bool) }),
+                    null, new HarmonyMethod(typeof(SemiRandom_Patch), nameof(ButtonText_Postfix)), null);
+            }
+            else
+            {
+                instance.Patch(AccessTools.PropertyGetter(typeof(ResearchProjectDef), "CanStartNow"),
+                    new HarmonyMethod(typeof(SemiRandom_Patch), nameof(CanStarNow_Prefix)), null, null);
+                instance.Patch(AccessTools.Method(typeof(Widgets), "ButtonText", new Type[] { typeof(Rect), typeof(string), typeof(bool), typeof(bool), typeof(bool), typeof(TextAnchor) }),
+                    null, new HarmonyMethod(typeof(SemiRandom_Patch), nameof(ButtonText_Postfix)), null);
+            }
             instance.Patch(AccessTools.Method(Type, "DrawResearchButton"),
                 null, new HarmonyMethod(typeof(SemiRandom_Patch), nameof(DrawResearchButton_Postfix)), null);
-
         }
 
         public static void DrawRightColumn_Prefix(object __instance)
@@ -41,6 +52,11 @@ namespace HumanResources
             selectedProject = researchProject;
         }
 
+        private static void CanStarNow_Prefix(ResearchProjectDef __instance)
+        {
+            selectedProject = __instance;
+        }
+
         private static bool ButtonText_Postfix(bool result)
         {
             if (!result || !Act || selectedProject == null) return result;
@@ -48,7 +64,7 @@ namespace HumanResources
             return false;
         }
 
-        private static void DrawResearchButton_Postfix(Rect drawRect, ResearchProjectDef projectDef)
+        public static void DrawResearchButton_Postfix(Rect drawRect, ResearchProjectDef projectDef)
         {
             float height = drawRect.height / 1.5f;
             Vector2 frameOffset = new Vector2(0, drawRect.yMax - height * 0.75f);
